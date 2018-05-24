a70bb7687fe79ee3487ee0accd8c046f39ba306b
Paige Ruppel <paige.ruppel@gmail.com>
2018-05-24 13:59:27 -0400

    added reset, hard, and easy functionality

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
 var colors = generateRandomColors(6);
 
-var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
-var colorDisplay = document.querySelector("#colorDisplay");
+var header = document.querySelector(".header");
 
+var reset = document.querySelector("#reset");
+var squares = document.querySelectorAll(".square");
+var colorDisplay = document.querySelector("#colorDisplay");
 var message = document.querySelector("#message");
 
-colorDisplay.textContent = pickedColor;
+
+var easyButton = document.querySelector("#easyBtn");
+var hardButton = document.querySelector("#hardBtn");
+
+var isHardMode = true;
+
+var colorToGuess = pickColor();
+
+
+
+easyButton.addEventListener("click", function() {
+	easyButton.classList.toggle("selected");
+	hardButton.classList.toggle("selected");
+	isHardMode = false;
+	resetForm(3);
+});
+
+hardButton.addEventListener("click", function() {
+	hardButton.classList.toggle("selected");
+	easyButton.classList.toggle("selected");
+	isHardMode = true;
+	resetForm(6);
+});
+
+colorDisplay.textContent = colorToGuess;
+
+reset.addEventListener("click", function() {
+	if (isHardMode) {
+	resetForm(6);
+	} else {
+	resetForm(3);
+	}
+});
+
+function resetForm(number) {
+	colors = generateRandomColors(number);
+	colorToGuess = pickColor();
+	colorDisplay.textContent = colorToGuess;	
+
+	for (var i = 0; i < squares.length; i++) {
+	var square = squares[i];
+		if (i < number) {
+			square.style.backgroundColor = colors[i];
+			square.classList.remove("hidden");
+		} else {
+			square.classList.add("hidden");
+		}
+	}
+
+	header.style.backgroundColor = "#232323";
+	reset.textContent = "New Colors";
+}
+
+
 
 for (var i = 0; i < squares.length; i++) {
 	var square = squares[i];
@@ -14,9 +68,11 @@ for (var i = 0; i < squares.length; i++) {
 
 	square.addEventListener("click", function() {
 		var clickedColor = this.style.backgroundColor;
-		if (clickedColor === pickedColor) {
+		if (clickedColor === colorToGuess) {
 			message.textContent = "Correct!";
 			changeColors(clickedColor);
+			header.style.backgroundColor = clickedColor;
+			reset.textContent = "Play Again?";
 		} else {
 		  this.style.backgroundColor = "#232323";
 		  message.textContent = "Please try again!";
@@ -45,13 +101,9 @@ function generateRandomColors(length) {
 }
 
 function generateRandomRgbValue() {
-		var values = [];
-		for (var i = 0; i < 3; i++) {
-			values[i] = generateRandomSingleValue();
-		}
-		return "rgb(" + values[0] + ", " + values[1] + ", " + values[2] + ")";
+	return "rgb(" + generateRandomSingleValue() + ", " + generateRandomSingleValue() + ", " + generateRandomSingleValue() + ")";
 }
 
 function generateRandomSingleValue() {
-	return Math.floor((Math.random() * 255 + 1));
+	return Math.floor((Math.random() * 266));
 }
