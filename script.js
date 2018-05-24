453a023696c7aadad0a5925fa02e2ce61671bd49
Paige Ruppel <paige.ruppel@gmail.com>
2018-05-24 12:05:37 -0400

    added random color functionality and click events on each of the colors to determine outcome of game

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(255, 0, 255)"
-	];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[4];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 
+var message = document.querySelector("#message");
+
 colorDisplay.textContent = pickedColor;
-for(var i = 0; i < squares.length; i++) {
-	squares[i].style.backgroundColor = colors[i];
+
+for (var i = 0; i < squares.length; i++) {
+	var square = squares[i];
+	square.style.backgroundColor = colors[i];
+
+	square.addEventListener("click", function() {
+		var clickedColor = this.style.backgroundColor;
+		if (clickedColor === pickedColor) {
+			message.textContent = "Correct!";
+			changeColors(clickedColor);
+		} else {
+		  this.style.backgroundColor = "#232323";
+		  message.textContent = "Please try again!";
+		}
+	});
+}
+
+function changeColors(color) {
+	for (var i = 0; i < squares.length; i++) {
+		var square = squares[i];
+		square.style.backgroundColor = color;
+	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(length) {
+	var arr = [];
+	for (var i = 0; i < length; i++) {
+		arr[i] = generateRandomRgbValue();
+	}
+	return arr;
+}
+
+function generateRandomRgbValue() {
+		var values = [];
+		for (var i = 0; i < 3; i++) {
+			values[i] = generateRandomSingleValue();
+		}
+		return "rgb(" + values[0] + ", " + values[1] + ", " + values[2] + ")";
+}
+
+function generateRandomSingleValue() {
+	return Math.floor((Math.random() * 255 + 1));
 }
